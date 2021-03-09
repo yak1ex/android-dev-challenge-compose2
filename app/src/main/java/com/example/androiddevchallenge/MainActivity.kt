7cde8a194869ed02a85c8b06d5182f0368b70556
Yak! <yak1ex@users.noreply.github.com>
2021-03-09 09:33:24 +0900

    Reflect linter results.

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -28,22 +28,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.androiddevchallenge.ui.theme.MyTheme
@@ -52,8 +52,8 @@ import com.example.androiddevchallenge.ui.theme.MyTheme
 class MainActivity : AppCompatActivity() {
     private val _counter = MutableLiveData(0)
     private val _active = MutableLiveData(false)
-    private val counter : LiveData<Int> = _counter
-    private val active : LiveData<Boolean> = _active
+    private val counter: LiveData<Int> = _counter
+    private val active: LiveData<Boolean> = _active
     private val handler = Handler(Looper.getMainLooper())
     private fun play() {
         val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
@@ -62,7 +62,7 @@ class MainActivity : AppCompatActivity() {
     }
     private val incrementCounter = object : Runnable {
         override fun run() {
-            if(_active.value!!) {
+            if (_active.value!!) {
                 if (_counter.value!! > 0) {
                     _counter.value = _counter.value!! - 1
                 }
